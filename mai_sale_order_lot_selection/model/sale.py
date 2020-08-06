0c5fc1ae72e0b44d6c35bec1825cdf4d9d232e12
doaa <doaakhaled6969@gmail.com>
2020-08-06 16:03:04 +0200

    [add]

--- mai_sale_order_lot_selection/model/sale.py
+++ mai_sale_order_lot_selection/model/sale.py
@@ -6,6 +6,13 @@ class SaleOrderLine(models.Model):
 
     lot_id = fields.Many2one('stock.production.lot', 'Lot', copy=False)
 
+    # doaa added
+    @api.onchange('product_id')
+    def onchange_product_id_changes_lot(self):
+        if self.product_id:
+            self.lot_id = self.env['stock.production.lot'].search(
+                                 [('product_id', '=', self.product_id.id)], limit=1).id
+
     # @api.multi
     def _prepare_procurement_values(self, group_id=False):
         res = super(SaleOrderLine, self)._prepare_procurement_values(group_id=group_id)
