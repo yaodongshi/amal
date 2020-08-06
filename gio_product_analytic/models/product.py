d9b449ef0e7f606506da1580df158109220ff5b1
doaa <doaakhaled6969@gmail.com>
2020-08-06 14:12:32 +0200

    [Add]

--- gio_product_analytic/models/product.py
+++ gio_product_analytic/models/product.py
@@ -7,7 +7,6 @@ from odoo import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-
     gio_analytic_account = fields.Many2one('account.analytic.account', string="ANALYTIC ACCOUNT")
     gio_analytic_tag = fields.Many2one('account.analytic.tag', string="ANALYTIC TAG", copy=False)
 
@@ -18,7 +17,8 @@ class ProductTemplate(models.Model):
         name = res.name
         default_code = res.default_code
         analytic_name = str(default_code) + '/' + name
-        res.gio_analytic_account = self.env['account.analytic.account'].create({'name': analytic_name}).id
+        res.gio_analytic_account = self.env['account.analytic.account'].create(
+            {'name': analytic_name, 'product_id': res.id}).id
         return res
 
     # added by doaa
@@ -27,25 +27,40 @@ class ProductTemplate(models.Model):
             name = vals['name']
             default_code = self.default_code
             analytic_name = default_code + '/' + name
+            if not self.gio_analytic_account.product_id:
+                self.gio_analytic_account.product_id = self.id
             if self.gio_analytic_account:
-               self.gio_analytic_account.name = analytic_name
+                self.gio_analytic_account.name = analytic_name
             else:
-                self.gio_analytic_account = self.env['account.analytic.account'].create({'name': analytic_name}).id
+                self.gio_analytic_account = self.env['account.analytic.account'].create(
+                    {'name': analytic_name, 'product_id': self.id}).id
 
         if 'name' not in vals and 'default_code' in vals:
             name = self.name
             default_code = vals['default_code']
             analytic_name = default_code + '/' + name
+            if not self.gio_analytic_account.product_id:
+                self.gio_analytic_account.product_id = self.id
             if self.gio_analytic_account:
-               self.gio_analytic_account.name = analytic_name
+                self.gio_analytic_account.name = analytic_name
             else:
-                self.gio_analytic_account = self.env['account.analytic.account'].create({'name': analytic_name}).id
+                self.gio_analytic_account = self.env['account.analytic.account'].create(
+                    {'name': analytic_name, 'product_id': self.id}).id
         if 'name' in vals and 'default_code' in vals:
             name = vals['name']
             default_code = vals['default_code']
             analytic_name = default_code + '/' + name
+            if not self.gio_analytic_account.product_id:
+                self.gio_analytic_account.product_id = self.id
             if self.gio_analytic_account:
-               self.gio_analytic_account.name = analytic_name
+                self.gio_analytic_account.name = analytic_name
             else:
-                self.gio_analytic_account = self.env['account.analytic.account'].create({'name': analytic_name}).id
+                self.gio_analytic_account = self.env['account.analytic.account'].create(
+                    {'name': analytic_name, 'product_id': self.id}).id
         return super(ProductTemplate, self).write(vals)
+
+# added by doaa
+class AccountAnalyticAccount(models.Model):
+    _inherit = "account.analytic.account"
+
+    product_id = fields.Many2one('product.template', string="Product")
